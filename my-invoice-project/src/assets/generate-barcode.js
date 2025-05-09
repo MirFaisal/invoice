7611abe267ea8885696dfab85a877234d9466dd3
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-05-09 23:22:19 +0600

    Refactor barcode and QR code generation scripts for consistent quotation style

--- my-invoice-project/src/assets/generate-barcode.js
+++ my-invoice-project/src/assets/generate-barcode.js
@@ -1,15 +1,15 @@
-const JsBarcode = require('jsbarcode');
-const { createCanvas } = require('canvas');
+const JsBarcode = require("jsbarcode");
+const { createCanvas } = require("canvas");
 
 const canvas = createCanvas();
-JsBarcode(canvas, '01684645670', {
-  format: 'CODE128',
+JsBarcode(canvas, "01684645670", {
+  format: "CODE128",
   width: 2,
   height: 100,
-  displayValue: false
+  displayValue: false,
 });
 
-const fs = require('fs');
-const out = fs.createWriteStream(__dirname + '/barcode.png');
+const fs = require("fs");
+const out = fs.createWriteStream(__dirname + "/barcode.png");
 const stream = canvas.createPNGStream();
 stream.pipe(out);
--- my-invoice-project/src/assets/generate-qr.js
+++ my-invoice-project/src/assets/generate-qr.js
@@ -1,25 +1,29 @@
-const QRCode = require('qrcode');
+const QRCode = require("qrcode");
 
 // Create QR code data with invoice information
 const invoiceData = {
-    mpId: '142970246',
-    invoiceNo: '51225',
-    phone: '01723251225',
-    amount: '1250'
+  mpId: "142970246",
+  invoiceNo: "51225",
+  phone: "01723251225",
+  amount: "1250",
 };
 
 const qrData = JSON.stringify(invoiceData);
 
 // Generate QR code as SVG
-QRCode.toString(qrData, {
-    type: 'svg',
+QRCode.toString(
+  qrData,
+  {
+    type: "svg",
     color: {
-        dark: '#000000',
-        light: '#ffffff'
-    }
-}, function (err, string) {
+      dark: "#000000",
+      light: "#ffffff",
+    },
+  },
+  function (err, string) {
     if (err) throw err;
-    
-    const fs = require('fs');
-    fs.writeFileSync(__dirname + '/qr-code.svg', string);
-});
+
+    const fs = require("fs");
+    fs.writeFileSync(__dirname + "/qr-code.svg", string);
+  },
+);
